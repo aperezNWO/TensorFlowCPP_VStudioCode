aa0111d6c6745e18cc69864be72966371a701e81
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-04-14 23:26:22 -0500

    setting working directory

---
+++ src/__tensorflow/_tensorflow_c/test_dll_exe.c
@@ -0,0 +1,36 @@
+/*
+
+gcc -o TensorFlowAppC.exe test_simple_dll.c -I"include" -L"lib" -ltensorflow
+
+*/
+#include <stdio.h>
+#include <windows.h>
+
+typedef const char* (*GetTFVersionFunc)();  // Define function pointer type
+
+int main() {
+    // Load the DLL
+    HMODULE hDLL = LoadLibrary("TensorFlowAppC.dll");
+    if (hDLL == NULL) {
+        printf("Failed to load DLL.\n");
+        return 1;
+    }
+
+    // Get the function from the DLL
+    GetTFVersionFunc GetTensorFlowVersion = (GetTFVersionFunc)GetProcAddress(hDLL, "GetTensorFlowVersion");
+    if (!GetTensorFlowVersion) {
+        printf("Could not locate the function.\n");
+        FreeLibrary(hDLL);
+        return 1;
+    }
+
+    // Call the function
+    const char* version = GetTensorFlowVersion();
+    printf("Returning Message : %s\n", version);
+
+    // Clean up
+    FreeLibrary(hDLL);
+    return 0;
+}
+
+
---
+++ src/__tesseract/_tesseract_cpp/simple_dll_test.cpp
@@ -0,0 +1,55 @@
+/*
+
+g++ -std=c++11 -Wall -Wextra -I"include" test_simple_dll.cpp -o test_simple_dll.exe -L"lib" -l:tensorflow.lib
+
+
+*/
+
+#include <iostream>
+#include <windows.h>
+
+// Define a function pointer type that matches the exported function signature
+typedef const char* (__cdecl *CallTFVersionFunc)();
+typedef const char* (__cdecl *CallOcrFunc)();
+
+int main() {
+    // Load the DLL
+    HINSTANCE hDLL = LoadLibraryA("TensorFlowApp64_CPP.dll"); // Replace "your_tensorflow_dll.dll" with the actual name of your DLL
+
+    if (hDLL == nullptr) {
+        std::cerr << "Error loading DLL: " << GetLastError() << std::endl;
+        return 1;
+    }
+ 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    CallOcrFunc callOcrFunc = (CallTFVersionFunc)GetProcAddress(hDLL, "GetTensorFlowOcrOutput");
+
+
+    if (callOcrFunc == nullptr) {
+        std::cerr << "Error getting function address for OCR: " << GetLastError() << std::endl;
+        FreeLibrary(hDLL);
+        return 1;
+    }
+
+    // Call the exported function
+    std::cout << "Calling OCR function from DLL..." << std::endl;
+
+    const char* ocrOutput = callOcrFunc();
+
+    if (ocrOutput != nullptr) {
+        std::cout << "OCR Output: " << ocrOutput << std::endl;
+    } else {
+        std::cerr << "Error: GetTensorFlowOcrOutput returned a null pointer." << std::endl;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Free the DLL
+    FreeLibrary(hDLL);
+
+    return 0;
+}
+
+
+
